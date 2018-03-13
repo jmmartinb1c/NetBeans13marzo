c89239faf320cd386796d7799007583133e19514
jmmartinb1c <jmmartinb1c@gmail.com>
2018-03-13 09:15:31 +0100

    Commit2 actualizacion 2 Jose Manuel

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -15,7 +15,7 @@ public class PracticaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola que tal?");
+        System.out.println("Hola que tal? Soy pepito");
     }
     
 }
